c50c5f6da113743ed5cbee2f148c1a3bfeccb987
Kristof Ringleff <kristof@fooman.co.nz>
2016-10-06 23:43:47 +1300

    0.8.14 Release

---
+++ app/code/community/Fooman/SpeedsterAdvanced/Block/Adminhtml/Extensioninfo.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * @author     Kristof Ringleff
+ * @package    Fooman_SpeedsterAdvanced
+ * @copyright  Copyright (c) 2009 Fooman Limited (http://www.fooman.co.nz)
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+class Fooman_SpeedsterAdvanced_Block_Adminhtml_Extensioninfo extends Fooman_Common_Block_Adminhtml_Extensioninfo
+{
+    protected $_hasSelftest = false;
+    protected $_idString = 'speedsterAdvanced';
+    protected $_moduleName = 'Fooman_SpeedsterAdvanced';
+}
